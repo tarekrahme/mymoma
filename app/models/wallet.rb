eab093049a9b62edcc7e10a33365b44ab51ebccc
Luca Kuhn <luca.kuhn@hotmail.de>
2019-05-24 11:15:31 +0100

    wallet controller and view

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -2,4 +2,14 @@ class Wallet < ApplicationRecord
   belongs_to :user
   has_many :goals
   has_many :transactions
+  before_action :set_wallet, only: [:show]
+
+  def show
+  end
+
+  private
+
+  def set_wallet
+    @wallet = Wallet.find(params[:id])
+  end
 end
