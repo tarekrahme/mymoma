356d802688f64168799c779c26232eca464d2934
Tijani Fulani <Fulani07@gmail.com>
2019-05-24 17:00:01 +0100

    made sure the links and the labels are working and correct

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
   get 'days/index'
   devise_for :users
   root to: 'pages#home'
-  get "/profile", to: "user#show"
+  get "/profile", to: "users#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, except: :index
-  resources :wallets, only: :show
+  resources :wallets, only: [:show, :create ]do
+    collection do
+      get 'tell_us_a_bit_more', to: 'wallets#tell_us_a_bit_more'
+      get 'recommendations', to: 'wallets#recommendations'
+    end
+  end
   resources :goals, except: :index
   resources :transactions, only: [:show, :index]
   resources :days , only: [:show, :index]
