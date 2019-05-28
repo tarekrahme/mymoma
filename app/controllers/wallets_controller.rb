7eff03128a3490f91aa99384733008336b223a75
Tijani Fulani <Fulani07@gmail.com>
2019-05-28 12:53:43 +0100

    updated the tell us more page

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,5 +1,5 @@
 class WalletsController < ApplicationController
-  before_action :set_wallet, only: [:show]
+  before_action :set_wallet, only: [:show, :update, :recommendations]
 
   def show
   end
@@ -8,10 +8,14 @@ class WalletsController < ApplicationController
     @wallet = current_user.wallet
   end
 
+  def update
+    @wallet.update(wallet_params)
+    redirect_to tell_us_a_bit_more_wallets_path
+  end
+
   def recommendations
+    @available_spend = @wallet.daily_income_cents - @wallet.savings_cents - @wallet.fixed_cost_cents
     @wallet = current_user.wallet
-    # raise
-    # @wallet.update(wallet_params)
   end
 
   private
@@ -21,6 +25,7 @@ class WalletsController < ApplicationController
   end
 
   def wallet_params
-    params.require(:wallet).permit(:daily_income, :savings, :fixed_cost)
+    # params.require(:wallet).permit(:daily_income, :savings, :fixed_cost)
+    params.require(:wallet).permit(:daily_income_cents, :savings_cents, :fixed_cost_cents)
   end
 end
