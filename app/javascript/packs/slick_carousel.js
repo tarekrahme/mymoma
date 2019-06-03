502c3e43fbb597b93e1babcfa07508b8a61c92c7
Luca Kuhn <luca.kuhn@hotmail.de>
2019-06-03 17:02:23 +0100

    slick carousel

---
+++ app/javascript/packs/slick_carousel.js
@@ -0,0 +1,10 @@
+import 'slick-carousel';
+
+$('.slick-carousel').slick({
+  prevArrow: '<i class="fas fa-chevron-left"></i>',
+  nextArrow: '<i class="fas fa-chevron-right"></i>',
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  autoplay: false
+});
