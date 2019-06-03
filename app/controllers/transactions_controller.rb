f65cdc7ff46a351ec1615b1a5ce1e3a9aec76810
Tarek Rahme <rahmetarek@gmail.com>
2019-06-03 15:04:48 +0100

    view pages

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,6 +4,7 @@ class TransactionsController < ApplicationController
   def index
     @transactions = Transaction.joins(:day).where(days: { date: Date.today }, wallet: current_user.wallet)
     @transactions_yesterday = Transaction.joins(:day).where(days: { date: Date.today - 1 }, wallet: current_user.wallet)
+    @transactions_2days = Transaction.joins(:day).where(days: { date: Date.today - 2 }, wallet: current_user.wallet)
   end
 
   def show
@@ -11,13 +12,14 @@ class TransactionsController < ApplicationController
 
   def new
     @transaction = Transaction.new
+    @transaction.build_day
   end
 
   def create
     @transaction = Transaction.new(amount_cents: transaction_params["amount_cents"].to_f * 100.0)
     @transaction.merchant = transaction_params["merchant"]
     @transaction.wallet = current_user.wallet
-    day = Day.find_by(date: transaction_params["day"]) || Day.create(date: transaction_params["day"], wallet: current_user.wallet, goal: current_user&.wallet&.goal)
+    day = Day.find_by(date: transaction_params[:day_attributes][:date]) || Day.create(date: transaction_params[:day_attributes][:date], wallet: current_user.wallet, goal: current_user&.wallet&.goal)
     @transaction.day = day
     if @transaction.save
       redirect_to transactions_path
@@ -30,7 +32,7 @@ class TransactionsController < ApplicationController
   end
 
   def update
-    day = Day.find_by(date: transaction_params[:day][:date]) || Day.create(date: transaction_params[:day][:date], wallet: current_user.wallet, goal: current_user.wallet.goal)
+    day = Day.find_by(date: transaction_params[:day_attributes][:date]) || Day.create(date: transaction_params[:day_attributes][:date], wallet: current_user.wallet, goal: current_user.wallet.goal)
     amount = transaction_params["amount_cents"].to_f
     if @transaction.update(amount_cents: amount, merchant: transaction_params["merchant"], day: day)
       redirect_to transaction_path(@transaction)
@@ -47,7 +49,7 @@ class TransactionsController < ApplicationController
   private
 
   def transaction_params
-    params.require(:transaction).permit(:amount_cents, :merchant, :day, day: [:date])
+    params.require(:transaction).permit(:amount_cents, :merchant, day_attributes: [:date])
   end
 
   def set_transaction
