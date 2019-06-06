1b994d509a09a3c96938eb4385459b97163cc82b
Tarek Rahme <rahmetarek@gmail.com>
2019-06-06 11:05:47 +0100

    changes to views and links

--- app/javascript/plugins/chart.js
+++ app/javascript/plugins/chart.js
@@ -1,73 +1,73 @@
-import Chart from "chart.js"
-import "chart.js/dist/Chart.min.css"
+import Chart from "chart.js";
+import "chart.js/dist/Chart.min.css";
 
-const transactionsData = JSON.parse(document.getElementById('transactions-data').dataset.transactions);
-const today = new Date().getDay();
+if (document.getElementById('transactions-data')) {
+  const transactionsData = JSON.parse(document.getElementById('transactions-data').dataset.transactions);
 
-const days = JSON.parse(document.getElementById('transactions-data').dataset.days);
-const data = []
+  const today = new Date().getDay();
 
-const weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+  const days = JSON.parse(document.getElementById('transactions-data').dataset.days);
+  const data = []
 
-const daysOnAxis = []
+  const weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
 
+  const daysOnAxis = []
 
-days.forEach((day) => {
-  data.push(transactionsData[day])
-  daysOnAxis.push(weekdays[day])
-});
-
-console.log(data);
-
-var ctx = document.getElementById('myChart');
-if (ctx) {
-  var ctxChart = ctx.getContext('2d');
-  var myChart = new Chart(ctxChart, {
-      type: 'bar',
-      data: {
-          labels: daysOnAxis,
-          datasets: [{
-              label: 'Amount spent',
-              data: data,
-              backgroundColor: [
-                  '#406aec',
-                  '#406aec',
-                  '#406aec',
-                  '#406aec',
-                  '#406aec',
-                  '#406aec',
-                  '#406aec'
-              ],
-              borderColor: [
-                  '#406aec',
-                  '#406aec',
-                  '#406aec',
-                  '#406aec',
-                  '#406aec',
-                  '#406aec',
-                  '#406aec'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-            xAxes: [{
-              gridLines: {
-                  display:false
-              }
-            }],
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                },
-                gridLines: {
-                  display:false
-                }
-            }]
-          }
-      }
+  days.forEach((day) => {
+    data.push(transactionsData[day])
+    daysOnAxis.push(weekdays[day])
   });
-}
 
-ctx.style.backgroundColor = '#EDF1FE';
+
+  var ctx = document.getElementById('myChart');
+  if (ctx) {
+    var ctxChart = ctx.getContext('2d');
+    var myChart = new Chart(ctxChart, {
+        type: 'bar',
+        data: {
+            labels: daysOnAxis,
+            datasets: [{
+                label: 'Amount spent',
+                data: data,
+                backgroundColor: [
+                    '#406aec',
+                    '#406aec',
+                    '#406aec',
+                    '#406aec',
+                    '#406aec',
+                    '#406aec',
+                    '#406aec'
+                ],
+                borderColor: [
+                    '#406aec',
+                    '#406aec',
+                    '#406aec',
+                    '#406aec',
+                    '#406aec',
+                    '#406aec',
+                    '#406aec'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+              xAxes: [{
+                gridLines: {
+                    display:false
+                }
+              }],
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  },
+                  gridLines: {
+                    display:false
+                  }
+              }]
+            }
+        }
+    });
+  }
+  ctx.style.backgroundColor = '#EDF1FE';
+}
